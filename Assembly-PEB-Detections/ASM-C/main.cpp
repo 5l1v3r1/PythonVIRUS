10eff01b7034bb955865873b35ebccc128977c30
b3mb4m <b3mb4m@tuta.io>
2015-10-09 20:54:24 +0300

    Update main.cpp

--- Assembly-PEB-Detections/ASM-C/main.cpp
+++ Assembly-PEB-Detections/ASM-C/main.cpp
@@ -1,17 +1,15 @@
 #include "processor.h"
 #include "isdebugged.h"
+#include "NtGlobalFlag.h"
 
 //Coded By B3mB4m
+//b3mb4m@tuta.io
 
 int main( void )
 {
-	if(processor() == 1)
-		//printf("Virual box detected ..\n");
-		exit(0);
-	else{
-		if(isdebugged() == 1)
-			//printf("Virual box detected ..\n");
-			exit(0);}
+	if(processor() == 1)	exit(0);
+	if(isdebugged() == 1)	exit(0);
+	if (NtGlobalFlag32() == 0x70)	exit(0);
 
 	printf("Virtual Box not detected !");
 	
