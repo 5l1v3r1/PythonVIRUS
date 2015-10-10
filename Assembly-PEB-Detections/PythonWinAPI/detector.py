f20830063822c6fb3c05e57d9f39a1d6dd223d82
b3mb4m <b3mb4m@tuta.io>
2015-10-10 12:48:57 +0300

    Update detector.py

--- Assembly-PEB-Detections/PythonWinAPI/detector.py
+++ Assembly-PEB-Detections/PythonWinAPI/detector.py
@@ -1,3 +1,4 @@
+import win32api
 import win32gui
 import win32con
 import os
@@ -11,9 +12,19 @@ For this, once again, the WinApi offers us a function called FindWindow().
 Giving it a string identifying the parent window name, the function returns a 
 window handle (HWND) on this window if it succeeds. Note that the string doesn't have 
 to be case sensitive.
+
+The second trick for olly 1 is the use of the function OutputDebugString(). 
+This api aims at sending the debugger a string for it to display it. 
+We found out that using this function a precise way makes olly 1 crash. 
+Thus the following code makes it crash. 
 """
 
+#Well I just add second trick to top of function.So, if olly 1 is presence on computer directly crash.
+#Otherwise, return None like useless line.
+
+
 def immunityANDollydbg():
+	win32api.OutputDebugString("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s")
 	#I did try on Immunity Debugger and worked(2015)
 	name = "Immunity Debugger - %s - [CPU - main thread, module %s]" % (os.path.basename(__file__),os.path.basename(__file__).split("exe")[0])
 	name2 = "OllyDbg - %s - [CPU - main thread, module %s]" % (os.path.basename(__file__),os.path.basename(__file__).split("exe")[0])
